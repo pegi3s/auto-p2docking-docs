9e3c589d93f3f0798480a309da78de0c56f423de
Jorge-Vieira <jbvieira@ibmc.up.pt>
2024-07-18 14:46:53 +0100

    Update evoppi_querier_options.rs

--- source/evoppi_querier_options.rs
+++ source/evoppi_querier_options.rs
@@ -1,1 +1,13 @@
+evoppi_q_species=
+
+Homo sapiens options
+
+
+    
+    evoppi_q_intdb=
+    evoppi_q_intmod=
+    evoppi_q_intpolyq=
+    evoppi_q_preddb=
+    evoppi_q_predmod=
+    evoppi_q_predpolyq=
 
